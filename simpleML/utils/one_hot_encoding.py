1d7ef9ea5c985b4099cc51a05885be57afa57ee2
ahmadalvi <ahmadalvi2018@gmail.com>
2026-01-24 22:47:14 -0500

    working on one hot encoding

--- simpleML/utils/one_hot_encoding.py
+++ simpleML/utils/one_hot_encoding.py
@@ -8,3 +8,9 @@ def one_hot_encode(labels, num_classes) -> list[list[int]]:
     Returns:
         list of list of int: One-hot encoded representation of the labels.
     """
+    one_hot = []
+    for label in labels:
+        vector = [0] * num_classes
+        vector[label] = 1
+        one_hot.append(vector)
+    return one_hot
